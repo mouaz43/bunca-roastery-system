427d226b2e80e1fc6c40c6a5c1340822b7634e44
mouaz43 <mouazmjr51@icloud.com>
2026-02-11 16:01:43 -0800

    Add analytics controller placeholder

---
+++ controllers/analyticsController.js
@@ -0,0 +1,8 @@
+// controllers/analyticsController.js
+// Dieser Controller liefert Statistiken zu Bestellungen, Produktion und Lager.
+
+// Überblick über Kennzahlen (Platzhalterfunktion)
+exports.getAnalyticsOverview = (req, res) => {
+  // Hier würden Daten aus Bestellungen, Produktion und Lager analysiert und aufbereitet.
+  res.json({ nachricht: 'Analyse-Übersicht wird hier angezeigt.' });
+};
