a5fe576c0cee16a73d40e3ab56a7bab6e6dde9dc
mouaz43 <mouazmjr51@icloud.com>
2026-02-19 15:25:28 +0100

    Update actionRoutes.js

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -3,19 +3,22 @@ const express = require("express");
 const router = express.Router();
 const actionController = require("../controllers/actionController");
 
+const wrap = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
+
 // Orders
-router.post("/orders/create", actionController.createOrder);
-router.post("/orders/:id/advance", actionController.advanceOrder);
-router.post("/orders/:id/approve", actionController.approveOrder);
-router.post("/orders/:id/deliver", actionController.deliverOrder);
-router.post("/orders/:id/delete", actionController.deleteOrder);
+router.post("/orders/create", wrap(actionController.createOrder));
+router.post("/orders/:id/advance", wrap(actionController.advanceOrder));
+router.post("/orders/:id/approve", wrap(actionController.approveOrder));
+router.post("/orders/:id/deliver", wrap(actionController.deliverOrder));
+router.post("/orders/:id/delete", wrap(actionController.deleteOrder));
 
 // Inventory
-router.post("/inventory/apply", actionController.applyInventoryChange);
+router.post("/inventory/apply", wrap(actionController.applyInventoryChange));
 
 // Batches
-router.post("/batches/create", actionController.createBatch);
-router.post("/batches/:id/advance", actionController.advanceBatch);
-router.post("/batches/:id/delete", actionController.deleteBatch);
+router.post("/batches/create", wrap(actionController.createBatch));
+router.post("/batches/:id/advance", wrap(actionController.advanceBatch));
+router.post("/batches/:id/delete", wrap(actionController.deleteBatch));
 
 module.exports = router;
