ae2fcb49d3ba10dee06f7d3e1be5452c680d7f23
mouaz43 <mouazmjr51@icloud.com>
2026-02-19 15:25:53 +0100

    Update pageRoutes.js

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -3,17 +3,18 @@ const express = require("express");
 const router = express.Router();
 const pageController = require("../controllers/pageController");
 
-router.get("/", pageController.renderHome);
-router.get("/dashboard", pageController.renderDashboard);
+const wrap = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
 
-router.get("/orders", pageController.renderOrders);
-router.get("/production", pageController.renderProduction);
-router.get("/inventory", pageController.renderInventory);
+router.get("/", wrap(pageController.renderHome));
+router.get("/dashboard", wrap(pageController.renderDashboard));
 
-router.get("/analytics", pageController.renderAnalytics);
-router.get("/settings", pageController.renderSettings);
+router.get("/orders", wrap(pageController.renderOrders));
+router.get("/production", wrap(pageController.renderProduction));
+router.get("/inventory", wrap(pageController.renderInventory));
 
-// NEW
-router.get("/activity", pageController.renderActivity);
+router.get("/analytics", wrap(pageController.renderAnalytics));
+router.get("/settings", wrap(pageController.renderSettings));
+router.get("/activity", wrap(pageController.renderActivity));
 
 module.exports = router;
